2739bbf634239e9903e35d1644a7b66f527d4577
LackupL58 <beekoop158+gh@gmail.com>
2026-02-02 04:04:56 -0700

    fix to work for each mod

--- ListUnits.sh
+++ ListUnits.sh
@@ -2,6 +2,7 @@
 
 created_units_here='./*/Data/World/Units/*/*.xml'
 each_mod_units_path='./*/Data/World/Units/'
+unit_path_middle='Data/World/Units/'
 wildcard_xml_suffix='/*.xml$'
 all_factions="$(find ./*/Data/World/Units/* -type d -exec basename {}  \; | sort | uniq)"
 templates_folder='./Backup Units + Inventory for All Units - Templates/'
@@ -10,9 +11,18 @@ destination_1='./Backup Units + Inventory for All Units (1)/'
 
 #echo $all_factions
 
-for faction in ${all_factions}; do
-  path=${each_mod_units_path}${faction}
-  #for file in $path; do
-  find ${path} ${wildcard_xml_suffix}
-  #done
+# */ checks only for folders
+for mod in */ ; do
+  # Excluding symlinks
+  [ -L "${d%/}" ] && continue
+  # Check each mod for units in factions
+  for faction in ${all_factions}; do
+    path=${mod}${unit_path_middle}${faction}
+    #echo ${path}
+    # If path exists
+    if [ -e "${path}" ]; then
+      #find ${path} ${wildcard_xml_suffix}
+      echo ${path}
+    fi
+  done
 done
